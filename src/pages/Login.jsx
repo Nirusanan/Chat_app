5a07cdaa0cf644c1cac977f62c091687a2a3567f
niru9711 <nirusanant97@gmail.com>
2024-04-21 01:06:17 +0530

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase';
 
 export default function Login() {
+
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
         <span className='logo'>Fast Chat</span>
         <span className='title'>Login</span>
 
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type='email' placeholder='Email'/>
             <input type='password' placeholder='Password'/>
             
             <button>Sign in</button>
+          {error && <span>Something went wrong</span>}
         </form>
-        <p>You don't have an account? Register</p>
+        <p>You don't have an account? <Link to="/register"> Register </Link></p>
 
       </div>
     </div>
