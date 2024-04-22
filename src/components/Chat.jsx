87b41bd4b4fe4fe3dd861731b462a14f2c5dd4ea
niru9711 <nirusanant97@gmail.com>
2024-04-23 00:22:23 +0530

    dev

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Add from '../img/add.png';
 import Cam from '../img/videocamera.png';
 import More from '../img/more.png';
 import Messages from './Messages';
 import Input from './Input';
+import { ChatContext } from '../context/ChatContext';
 
 
 export default function Chat() {
+  const { data } = useContext(ChatContext);
+  console.log(data);
   return (
     <div className='chat'>
       <div className="chatInfo">
-        <span>Niru</span>
+        <span>{data.user.displayName}</span>
         <div className="chatIcons">
           <img src={Cam} alt='' title='camera'/>
           <img src={Add} alt='' title='add'/>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,40 +1,78 @@
-import React, { useState } from 'react';
-import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useContext, useState } from 'react';
+import { collection, getDocs, query, where, doc, setDoc, getDoc, updateDoc, serverTimestamp } from "firebase/firestore";
 import { db } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 export default function Search() {
   const [userName, setUserName] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
 
-  const handleSearch = async () =>{
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async () => {
     const q = query(
-      collection(db, "users"), 
+      collection(db, "users"),
       where("displayName", "==", userName)
     );
 
-    try{
+    try {
       const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) =>{
+      querySnapshot.forEach((doc) => {
         setUser(doc.data());
-      })  ;
-    }catch(err){
+      });
+    } catch (err) {
       setErr(true);
     }
   };
 
-  const handleKey = (e) =>{
+  const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
-  }
+  };
+
+  const handleSelect = async () => {
+    const combinedID = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, 'chats', combinedID));
+
+      if (!res.exists()) {
+        // create a chat in chats collection
+        await setDoc(doc(db, 'chats', combinedID), { messages: [] });
+
+        // create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedID + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL
+          },
+          [combinedID + ".date"]: serverTimestamp()
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedID + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+          },
+          [combinedID + ".date"]: serverTimestamp()
+        });
+
+      }
+    } catch (err) {}
+
+    setUser(null);
+    setUserName('');
+  };
 
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type='text' placeholder='Find a user' onKeyDown={handleKey} onChange={e=>setUserName(e.target.value)}/>
+        <input type='text' placeholder='Find a user' onKeyDown={handleKey} onChange={e => setUserName(e.target.value)} value={userName}/>
       </div>
       {err && <span>User not found</span>}
 
-      {user && <div className="userChat">
+      {user && <div className="userChat" onClick={handleSelect}>
         <img src={user.photoURL} alt='' />
         <div className="userChatInfo">
           <span>
